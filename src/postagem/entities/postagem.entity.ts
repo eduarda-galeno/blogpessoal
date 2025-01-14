751cc6aa600ddbf5c492f41f39b18691c9ccbcd9
Eduarda <contatoeduardagaleno@outlook.com>
2025-01-14 20:58:14 -0300

    Criação do CRUD

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,3 +1,4 @@
+import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
 import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn} from "typeorm";
 
@@ -7,10 +8,12 @@ export class Postagem{
     @PrimaryGeneratedColumn() //id INT AUTO_INCREMENT PRIMATY KEY
     id: number;
 
+    @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsNotEmpty() //Validação dos dados do objeto 
     @Column({length: 100, nullable: false}) //VARCHAR(100) NOT NULL
     titulo: string;
 
+    @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsNotEmpty() //Validação dos dados do objeto 
     @Column({length: 1000, nullable: false}) //VARCHAR(100) NOT NULL
     texto: string;
